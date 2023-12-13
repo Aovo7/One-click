e3060f3f52c3a5a0038e68c27b9c889c9d0d9342
Aovo7 <134157338+Aovo7@users.noreply.github.com>
2023-12-13 22:46:19 +0800

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -2,7 +2,7 @@
 #安装weget curl依赖包
 #yum update -y && yum install curl -y #CentOS/Fedora
 #apt-get update -y && apt-get install curl -y #Debian/Ubuntu
-
+#远程下载代码curl -sS -O https://raw.githubusercontent.com/Aovo7/Oneclick/main/7.sh && chmod +x 7.sh && ./7.sh
 while true; do
 clear
 echo "------------------------"
@@ -32,6 +32,9 @@ read -t 0.1 -n 10000
 read -p "请输入数字或指令: " choice
 
 case $choice in
+	renew)
+		curl -sS -O https://raw.githubusercontent.com/Aovo7/Oneclick/main/7.sh && chmod +x 7.sh && ./7.sh
+  		;;
 	1) 
 		clear
 		echo "收集 VPS 系统信息中..."
